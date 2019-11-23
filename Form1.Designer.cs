92301e4ef6ffb620d92864b7dfba2eba8ed7c663
Lukasz Ciesielski <lukasz7446@gmail.com>
2019-11-23 19:21:56 +0100

    Implementation

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FoldersToCBZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openDirectoryDialogButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openDirectoryDialogButton
+            // 
+            this.openDirectoryDialogButton.Location = new System.Drawing.Point(12, 25);
+            this.openDirectoryDialogButton.Name = "openDirectoryDialogButton";
+            this.openDirectoryDialogButton.Size = new System.Drawing.Size(127, 85);
+            this.openDirectoryDialogButton.TabIndex = 0;
+            this.openDirectoryDialogButton.Text = "Choose directory with folders containing graphic files";
+            this.openDirectoryDialogButton.UseVisualStyleBackColor = true;
+            this.openDirectoryDialogButton.Click += new System.EventHandler(this.openDirectoryDialogButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "This tool will convert folders containing graphic files into CBZ files";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 131);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(10, 13);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 194);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.openDirectoryDialogButton);
+            this.Name = "Form1";
+            this.Text = "Folders to CBZ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openDirectoryDialogButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace FoldersToCBZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openDirectoryDialogButton_Click(object sender, EventArgs e)
+        {
+            this.openDirectoryDialogButton.Enabled = false;
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() != CommonFileDialogResult.Ok)
+                return;
+
+            Task.Run(() =>
+            {
+                var directories = Directory.GetDirectories(dialog.FileName);
+
+                for (int currentDirectoryIndex = 0; currentDirectoryIndex < directories.Length; currentDirectoryIndex++)
+                {
+                    this.Invoke((MethodInvoker)delegate { statusLabel.Text = $"Working: {currentDirectoryIndex} / {directories.Length}."; });
+                    try
+                    {
+                        ZipFile.CreateFromDirectory(directories[currentDirectoryIndex], directories[currentDirectoryIndex] + ".cbz");
+                    }
+                    catch (IOException exception)
+                    {
+                        MessageBox.Show(exception.Message);
+                        this.Invoke((MethodInvoker)delegate 
+                        {
+                            statusLabel.Text = $"Failed";
+                            this.openDirectoryDialogButton.Enabled = true;
+                        });
+
+                        return;
+                    }
+                }
+
+                this.Invoke((MethodInvoker)delegate 
+                {
+                    statusLabel.Text = $"Done: {directories.Length} / {directories.Length}.";
+                    this.openDirectoryDialogButton.Enabled = true;
+                });
+
+            });
+        }
+    }
+}
